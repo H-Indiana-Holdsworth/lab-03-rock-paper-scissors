a609b0cfc9254d79bba39f995b4692463dafb22a
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-06 14:25:33 -0700

    Adds HTML and CSS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // import functions and grab DOM elements
 
-// initialize global state
 
+const shootButton = document.getElementById('shoot-button');
+const winMessage = document.getElementById('win-message');
+const lossMessage = document.getElementById('loss-message');
+// initialize global state
+let wins = 0;
+let losses = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
